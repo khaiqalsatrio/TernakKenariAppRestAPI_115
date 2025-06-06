0c82fc276feac0a25dca56696bf5e511edf3b7c9
Khaiqol <Khaiqol@gmail.com>
2025-06-07 01:42:31 +0700

    get burung tersedia bloc

---
+++ lib/presentation/bloc/get_all_burung_tersedia/get_burung_tersedia_bloc.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:project_115/data/repository/get_all_burung_tersedia_repository.dart';
+import 'package:project_115/presentation/bloc/get_all_burung_tersedia/get_burung_tersedia_event.dart';
+import 'package:project_115/presentation/bloc/get_all_burung_tersedia/get_burung_tersedia_state.dart';
+
+class GetBurungTersediaBloc
+    extends Bloc<GetBurungTersediaEvent, GetBurungTersediaState> {
+  final GetAllBurungTersediaRepository getAllBurungTersediaRepository;
+
+  GetBurungTersediaBloc(this.getAllBurungTersediaRepository)
+    : super(GetBurungTersediaInitial()) {
+    on<GetAllBurungTersediaEvent>(_getAllBurungTersedia);
+  }
+
+  Future<void> _getAllBurungTersedia(
+    GetAllBurungTersediaEvent event,
+    Emitter<GetBurungTersediaState> emit,
+  ) async {
+    emit(GetBurungTersediaLoading());
+    final result = await getAllBurungTersediaRepository.getAllBurungTersedia();
+    result.fold(
+      (error) => emit(GetBurungTersediaError(message: error)),
+      (burungTersedia) =>
+          emit(GetBurungTersediaLoaded(burungTersedia: burungTersedia)),
+    );
+  }
+}
