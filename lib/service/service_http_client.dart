95ca4031875c3207114430b75385dbf0a5703d91
Khaiqol <Khaiqol@gmail.com>
2025-06-07 01:43:10 +0700

    servive

--- lib/service/service_http_client.dart
+++ lib/service/service_http_client.dart
@@ -1,15 +1,15 @@
 import 'dart:convert';
-
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class ServiceHttpClient {
-  final String baseUrl = 'http://10.0.0.2:8000/api/';
-  final secureStorage = const FlutterSecureStorage();
+  final String baseUrl = 'http://10.0.2.2:8000/api/';
+  final secureStorage = FlutterSecureStorage();
 
-  // POST request
+  //POST
   Future<http.Response> post(String endpoint, Map<String, dynamic> body) async {
     final url = Uri.parse("$baseUrl$endpoint");
+
     try {
       final response = await http.post(
         url,
@@ -25,10 +25,11 @@ class ServiceHttpClient {
     }
   }
 
-  // GET request
+  //GET
   Future<http.Response> get(String endpoint) async {
     final token = await secureStorage.read(key: "authToken");
     final url = Uri.parse("$baseUrl$endpoint");
+
     try {
       final response = await http.get(
         url,
@@ -42,5 +43,18 @@ class ServiceHttpClient {
     } catch (e) {
       throw Exception("GET request failed: $e");
     }
+  }
+
+  Future<http.Response> postWithToken(String url, dynamic body) async {
+    final token = await secureStorage.read(key: "authToken");
+
+    return http.post(
+      Uri.parse(baseUrl + url),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+      body: body is String ? body : json.encode(body),
+    );
   }
 }
