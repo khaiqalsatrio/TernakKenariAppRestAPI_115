7151216a2dd9c547b5e25f1633181ad305646ad3
Khaiqol <Khaiqol@gmail.com>
2025-06-07 01:45:40 +0700

    fix buyer profile response

--- lib/data/model/response/buyer/buyer_profile_response_model.dart.dart
+++ lib/data/model/response/buyer/buyer_profile_response_model.dart.dart
@@ -1,37 +1,91 @@
 import 'dart:convert';
 
 class BuyerProfileResponseModel {
-  final int? burungId;
-  final String? burungType;
-  final int? harga;
-  final String? deskripsi;
+  String message;
+  int statusCode;
+  Data data;
 
   BuyerProfileResponseModel({
-    this.burungId,
-    this.burungType,
-    this.harga,
-    this.deskripsi,
+    required this.message,
+    required this.statusCode,
+    required this.data,
   });
 
-  factory BuyerProfileResponseModel.fromJson(String str) =>
-      BuyerProfileResponseModel.fromMap(json.decode(str));
+  BuyerProfileResponseModel copyWith({
+    String? message,
+    int? statusCode,
+    Data? data,
+  }) => BuyerProfileResponseModel(
+    message: message ?? this.message,
+    statusCode: statusCode ?? this.statusCode,
+    data: data ?? this.data,
+  );
 
-  String toJson() => json.encode(toMap());
+  factory BuyerProfileResponseModel.fromRawJson(String str) =>
+      BuyerProfileResponseModel.fromJson(json.decode(str));
 
-  factory BuyerProfileResponseModel.fromMap(Map<String, dynamic> json) =>
+  String toRawJson() => json.encode(toJson());
+
+  factory BuyerProfileResponseModel.fromJson(Map<String, dynamic> json) =>
       BuyerProfileResponseModel(
-        burungId: json["burung_id"],
-        burungType: json["burung_type"],
-        harga: json["harga"],
-        deskripsi: json["deskripsi"],
+        message: json["message"],
+        statusCode: json["status_code"],
+        data: Data.fromJson(json["data"]),
       );
 
-  get data => null;
+  Map<String, dynamic> toJson() => {
+    "message": message,
+    "status_code": statusCode,
+    "data": data.toJson(),
+  };
+}
 
-  Map<String, dynamic> toMap() => {
-    "burung_id": burungId,
-    "burung_type": burungType,
-    "harga": harga,
-    "deskripsi": deskripsi,
+class Data {
+  int id;
+  String name;
+  String address;
+  String phone;
+  String? photo;
+
+  Data({
+    required this.id,
+    required this.name,
+    required this.address,
+    required this.phone,
+    required this.photo,
+  });
+
+  Data copyWith({
+    int? id,
+    String? name,
+    String? address,
+    String? phone,
+    String? photo,
+  }) => Data(
+    id: id ?? this.id,
+    name: name ?? this.name,
+    address: address ?? this.address,
+    phone: phone ?? this.phone,
+    photo: photo ?? this.photo,
+  );
+
+  factory Data.fromRawJson(String str) => Data.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+    id: json["id"],
+    name: json["name"],
+    address: json["address"],
+    phone: json["phone"],
+    photo: json["photo"] ?? "", // Handle null case for photo
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "address": address,
+    "phone": phone,
+    "photo": photo,
   };
 }
